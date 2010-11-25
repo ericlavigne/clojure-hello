23e53dd5ad5f444e8f219925417ba78d89c0b501
Eric Lavigne <lavigne.eric@gmail.com>
2010-11-25 18:20:57 -0500

    Changed groupid because Clojars won't let me use my username with putting org.clojars before it. Changed tabs to spaces.

--- src/clojure_hello/core.clj
+++ src/clojure_hello/core.clj
@@ -1,22 +1,22 @@
 (ns clojure-hello.core
-  (:use compojure.core 
-	compojure.route
-	hiccup.core 
-	hiccup.page-helpers
-	ring.adapter.jetty))
+  (:use compojure.core
+        compojure.route
+        hiccup.core 
+        hiccup.page-helpers
+        ring.adapter.jetty))
 
 (defn layout [content]
   (html [:html
-	 [:head
-	  [:title "Hello world application"]]
-	 [:body
-	  content
-	  [:br]
-	  [:br]
-	  [:div
-	   "View the source code for this application on "
-	   (link-to "http://github.com/ericlavigne/clojure-hello"
-		    "Github.")]]]))
+         [:head
+          [:title "Hello world application"]]
+         [:body
+          content
+          [:br]
+          [:br]
+          [:div
+           "View the source code for this application on "
+           (link-to "http://github.com/ericlavigne/clojure-hello"
+                    "Github.")]]]))
 
 (defn index []
   (layout [:p "Hello world!"]))
--- test/clojure_hello/test/core.clj
+++ test/clojure_hello/test/core.clj
@@ -4,9 +4,9 @@
 
 (deftest says-hello
   (let [request {:uri "/" :request-method :get}
-	response (app request)]
+        response (app request)]
     (is (= 200 (:status response))
-	"Expect status 200 (success) for GET /")
+        "Expect status 200 (success) for GET /")
     (is (.contains (:body response) "Hello")
-	"Response page should contain the greeting 'Hello'.")))
+        "Response page should contain the greeting 'Hello'.")))
 
